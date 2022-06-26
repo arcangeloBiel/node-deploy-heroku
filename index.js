e4932b799a501dc60355ddb3372bd4726f67be18
João Azevedo Arcangelo Ribeiro <37083709+arcangeloBiel@users.noreply.github.com>
2022-06-26 15:50:02 -0300

    adiciona novas regras

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const app = express();
  const port = process.env.PORT || 3000;
 
  const lista = [ 
-      {Codigo: 1,  NomeRegra: 'Encarte',TipoRegra:  1, 'listaFamilia': ["Maculino", "Femenino"]}
+      {Codigo: 1,  NomeRegra: 'Encarte',TipoRegra:  1, 'listaFamilia': ["Maculino", "Femenino"]},
+      {Codigo: 2,  NomeRegra: 'promocão',TipoRegra:  14, 'listaFamilia': ["patricia foster", "langerie"]},
+      {Codigo: 3,  NomeRegra: 'Basico',TipoRegra:  19, 'listaFamilia': ["Nico boco", "infantil"]}
     ]
-
-
  app.get("/", function (req, res) {
-   
        res.send(lista);
  });
+ 
  app.listen(port, () => {
     console.log("Aplicação rodando em http://localhost:3000")
  })
